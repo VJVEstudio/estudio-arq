c3d1e1abdd39e7e713efefc09b75e48ae2ca3430
VJVEstudio <estudio@vjvarq.com>
2026-06-12 13:06:46 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,5 +34,5 @@ app.use((err, req, res, _next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🏥 Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
